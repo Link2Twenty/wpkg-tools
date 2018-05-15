e1fde17395f209020fe106dfca81a02a672f25d2
Andrew Bone <AndrewB05@gmail.com>
2018-05-15 15:12:10 +0100

    initial commit

---
+++ wpkg-variables.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+# Administrator details for site
+our $user = "administrator%password";
+
+# Location of WPKG reports on local server
+our $wpkg_reports = "/location/of/wpkgreports";
